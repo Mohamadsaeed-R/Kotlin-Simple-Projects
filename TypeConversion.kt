cf7979279f6cba759209b05b9bc4c0c804bc934a
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2025-10-27 07:47:32 +0000

    Add files via upload

---
+++ TypeConversion.kt
@@ -0,0 +1,19 @@
+package com.example.project1
+/*
+toByte()
+toShort()
+toInt()
+toLong()
+toFloat()
+toDouble()
+ */
+fun main(args: Array<String>) {
+
+    var a:Byte=127
+    var b:Int=a.toInt()
+    var c:Double=b.toDouble()
+    println(a)
+    println(b)
+    println(c)
+
+}
