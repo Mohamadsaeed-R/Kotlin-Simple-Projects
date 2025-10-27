17ef8bf280144d97ab6fe620816fcab547468ea6
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2025-10-27 07:00:10 +0000

    Add files via upload

---
+++ variables.kt
@@ -0,0 +1,22 @@
+package com.example.project1
+
+import java.time.Year
+
+fun main (args:Array<String>){
+
+    var a =10
+    var b =20
+    var c =a+b
+    println(c)
+    val d=10
+    val e=20
+    val f=d+e
+    println(e)
+    var Year=32
+    Year=33
+    println(Year)
+    val Month =1 // val cannot be reassigned.
+   
+
+
+}
