f8d58ec85ac0bc0add3a7f9799d0ca21b9341bd6
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2025-10-27 07:29:27 +0000

    Add files via upload

---
+++ DataTypes.kt
@@ -0,0 +1,17 @@
+package com.example.project1
+/*Defining variables in kotlin :
+ var name :DataTypes =Value
+In Kotlin, all data types are actually classes whose names start with a capital letter
+ */
+fun main(args:Array<String>){
+    var a:Boolean =true
+    var b:Char='R'
+    var c:Byte=12
+    var d:Short=356
+    var e:Long=-4564687
+    var f:Int=356
+    var h:Float= 3.14f//f is important
+    var g:Double=3.151592
+
+
+}
